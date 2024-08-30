816bf678f9e32faedb6e48485d54ac9307296b59
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-08-30 10:47:24 +0300

    db done

---
+++ utils/db.js
@@ -0,0 +1,49 @@
+// utils/db.js
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url, {
+      useUnifiedTopology: true,
+    });
+    this.client.connect().then(() => {
+      this.db = this.client.db(database);
+    }).catch((err) => {
+      console.error('MongoDB connection error:', err);
+    });
+  }
+
+  // Check if the MongoDB connection is alive
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  // Get the number of documents in the 'users' collection
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  // Get the number of documents in the 'files' collection
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+
+  // reference to users collection
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
+
+  // reference to file collections
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
+}
+
+// Create and export an instance of DBClient
+const dbClient = new DBClient();
+module.exports = dbClient;
